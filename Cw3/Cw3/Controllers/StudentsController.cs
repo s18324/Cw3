7ed9c39626dff2c8fcb020a16f0d3812f0105f25
s18324 <61690852+s18324@users.noreply.github.com>
2020-03-18 20:44:36 +0100

    Add files via upload

---
+++ Cw3/Cw3/Controllers/StudentsController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Cw3.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Cw3.Controllers
+{
+    [ApiController]
+    [Route("api/students")]
+    public class StudentsController : ControllerBase
+    {
+        //Zadanie 4
+        
+        // [HttpGet]
+        // public string GetStudent()
+        // {
+        //     return "Kowalski, Nowak, Matejko";
+        // }
+        
+        //Zadanie 5
+
+        // public string GetStudents(string orderBy)
+        // {
+        //     return $"Kowalski, Nowak, Matejko sortowanie={orderBy}";
+        // }
+
+        //zadanie 6
+        [HttpPost]
+        public IActionResult CreateStudent(Student student)
+        {
+            //add to database
+            //generating index nr
+            student.IndexNumber = $"s{new Random().Next(1, 20000)}";
+            return Ok(student);
+        }
+        
+    }
+}
