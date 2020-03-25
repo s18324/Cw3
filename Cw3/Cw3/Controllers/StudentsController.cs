a525fb6c32e005caf94fbfa128f0f7e25c6bfc38
s18324 <s18324@pjwstk.edu.pl>
2020-03-25 18:09:20 +0100

    Ćwiczenia 4- zmiany w HttpGet

--- Cw3/Cw3/Controllers/StudentsController.cs
+++ Cw3/Cw3/Controllers/StudentsController.cs
@@ -58,10 +58,47 @@ namespace Cw3.Controllers
             _dbService = dbService;
         }
 
-        [HttpGet]
-        public IActionResult GetStudents(string orderBy)
-        {
-            return Ok(_dbService.GetStudents());
+        //[HttpGet]
+        //public IActionResult GetStudents(string orderBy)
+        //{
+        //    return Ok(_dbService.GetStudents());
+        //}
+
+        //[HttpGet("{id}")]
+        //public IActionResult GetStudents(string orderBy){
+
+        [HttpGet("{id}")]
+        public IActionResult GetStudents(string id){
+            var list = new List<Object>();
+            using(var client= new SqlConnection("Data Source=db-mssql;Initial Catalog=s18324;Integrated Security=True")){
+                using (var command = new SqlCommand())
+            {
+                command.Connection = client;
+
+                command.CommandText =   "select e.Semester Semester, " +
+                                        "st.Name StudiesName from Student s" +
+                                        "inner join Enrollment e on s.IdEnrollment=e.IdEnrollment" +
+                                        "inner join Studies st on e.IdStudy=st.IdStudy"+
+                                        $"where s.IndexNumber=@id";
+                command.Parameters.AddWithValue("id", id);
+
+
+                client.Open();
+                var reader = command.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    var studia = new
+                    {
+                        Semestr = reader["Semester"].ToString(),
+                        StudiesName = reader["StudiesName"].ToString()
+                    };
+                    list.Add(studia);
+                }
+            }
+
+            return Ok(list);
+            }
         }
 
     }
